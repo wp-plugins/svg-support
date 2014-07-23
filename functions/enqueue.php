a9dd614c913a963cb83436fee5c3f9b826f024de
Benbodhi <Benbodhi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-23 10:58:21 +0000

    deleted 1.0 trunk files to make way for 2.0 files
    git-svn-id: https://plugins.svn.wordpress.org/svg-support/trunk@953389 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- functions/enqueue.php
+++
@@ -1,8 +0,0 @@
-<?php
-add_action( 'wp_enqueue_scripts', 'bodhi_gc_svg_inline' );
-
-function bodhi_gc_svg_inline() {
-	wp_register_script('bodhi_gc_svg_inline', plugins_url('svg-support/js/svg-inline.js'), array(), '1.0.0', false );
-	wp_enqueue_script('bodhi_gc_svg_inline');
-}
-?>
--- functions/mime-types.php
+++
@@ -1,8 +0,0 @@
-<?php
-add_filter( 'upload_mimes', 'bodhi_gc_upload_mimes' );
-
-function bodhi_gc_upload_mimes($mimes = array()) {
-	$mimes['svg'] = 'image/svg+xml';	// allow SVG file upload
-	return $mimes;
-}
-?>
