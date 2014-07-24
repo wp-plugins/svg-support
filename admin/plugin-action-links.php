f20f20987904dcf2da05e6480f94dacf50391742
Benbodhi <Benbodhi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-24 00:53:09 +0000

    tagged 2.1.3 for release - added plugin action links file
    git-svn-id: https://plugins.svn.wordpress.org/svg-support/tags/2.1.3@953833 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/plugin-action-links.php
@@ -0,0 +1,17 @@
+<?php
+/********************************************
+* PLUGIN ACTION LINKS
+********************************************/
+
+// add filter to svg-support only
+add_filter( 'plugin_action_links_' . plugin_basename(__FILE__), 'bodhi_svgs_plugin_action_links' );
+
+function bodhi_svgs_plugin_action_links( $links ) {
+
+   $links[] = '<a href="'. get_admin_url(null, 'options-general.php?page=svg-support') .'">Settings</a>';
+   //$links[] = '<a href="http://gowebben.com" target="_blank">More plugins by GoWebben</a>';
+   return $links;
+   
+}
+
+?>
